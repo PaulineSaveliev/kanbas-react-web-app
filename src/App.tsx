3d4c2fd36e9124315037c1821647c03550700e89
Pauline Saveliev <paulinesaveliev@yahoo.com>
2025-05-10 22:09:25 +0100

    Committed work from local file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// Pauline Saveliev
+// CS5610 - Summer 1 2025
+
+import Labs from "./Labs"
+
+export default function App() {
+  return (
+    <div>
+      <Labs />
+    </div>
+  );
+}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+// Pauline Saveliev
+// CS5610 - Summer 1 2025
+
+import Lab1 from "./Lab1";
+export default function Labs() {
+    return (
+        <div id="wd-labs">
+            <h1>Labs</h1>
+            <Lab1 />
+        </div>
+    )
+}
