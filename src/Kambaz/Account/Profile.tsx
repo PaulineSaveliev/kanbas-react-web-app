839b4bb55a2ba625af8281873989d664046bc22d
Pauline Saveliev <paulinesaveliev@yahoo.com>
2025-06-07 12:58:48 -0700

    account RESTful

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
+import * as client from "./client";
 
 export default function Profile() {
   const [profile, setProfile] = useState<any>({});
@@ -16,10 +17,16 @@ export default function Profile() {
     if (!currentUser) return navigate("/Kambaz/Account/Signin");
     setProfile(currentUser);
   }
-  const signout = () => {
+  const signout = async () => {
+    await client.signout();
     dispatch(setCurrentUser(null));
     navigate("/Kambaz/Account/Signin");
   };
+  const updateProfile = async () => {
+    const updatedProfile = await client.updateUser(profile);
+    dispatch(setCurrentUser(updatedProfile));
+  }
+
   useEffect(() => { fetchProfile(); }, []);
 
   return (
@@ -47,6 +54,7 @@ export default function Profile() {
             <option value="FACULTY">Faculty</option>
             <option value="STUDENT">Student</option>
           </select>
+          <button onClick={updateProfile} className="btn btn-primary w-100 mb-2">Update</button>
           <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
             Sign out
           </Button>
--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -2,21 +2,31 @@
 // CS5610 - Summer 1 2025
 
 // import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import * as client from "./client";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
 
 export default function Signup() {
+  const [user, setUser] = useState<any>({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const signup = async () => {
+    const currentUser = await client.signup(user);
+    dispatch(setCurrentUser(currentUser));
+    navigate("/Kambaz/Account/Profile");
+  }
   return (
     <div id="wd-signup-screen">
       <h3>Sign Up</h3>
       <Form>
-          <Form.Control className="wd-signin-input" type="text" placeholder="username" />
-          <Form.Control className="wd-signin-input" type="password" placeholder="password" />
-          <Link to="/Kambaz/Account/Profile" id="wd-signup-btn">
-            <Button className="wd-signin-input">
-              Sign Up
-            </Button>
-          </Link>
+          <Form.Control onChange={(e) => setUser({...user, username: e.target.value})} className="wd-signin-input" type="text" placeholder="username" />
+          <Form.Control onChange={(e) => setUser({...user, password: e.target.value})} className="wd-signin-input" type="password" placeholder="password" />
+          <Button className="wd-signin-input" onClick={signup}>
+            Sign Up
+          </Button>
       </Form>
       <Link  to="/Kambaz/Account/Signin" >Sign In</Link>
     </div>
